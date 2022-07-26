c3a2f4b47ecc8371c8c07a23a9d5e3363d4b05aa
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-07-26 11:37:36 +0545

    init : backend initiated with express server

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
 // not using express framework
-const http = require("http")
+// const http = require("http")
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type" : "text/plain"})
-    res.end("Hello world")
-})
+// const app = http.createServer((req, res) => {
+//     res.writeHead(200, {"Content-Type" : "text/plain"})
+//     res.end("Hello world")
+// })
 
-const Port = 5000;
-
-app.listen(Port, () => {
-    console.log(`Server running on port http://localhost:${Port}`)
-})
-
-
-// using express framework
-// const express = require("express");
-// const dotenv = require("dotenv");
-
-// dotenv.config();
-
-// const app = express();
-
-// const Port = process.env.PORT || 5000;
-
-// app.get("/", (req, res) => {
-//     res.send("hello world")
-// });
-
+// const Port = 5000;
 
 // app.listen(Port, () => {
 //     console.log(`Server running on port http://localhost:${Port}`)
-// });
+// })
+
+
+// using express framework
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+const Port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("hello world")
+});
+
+
+app.listen(Port, () => {
+    console.log(`Server running on port http://localhost:${Port}`)
+});
 
