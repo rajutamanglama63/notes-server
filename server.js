b82dc93b5df5b28efb3c356f062bf8b547282605
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-07-26 15:21:15 +0545

    new : middleware concept learned

--- server.js
+++ server.js
@@ -24,8 +24,20 @@ const app = express();
 
 const Port = process.env.PORT || 5000;
 
+
+// all this use things are middlewares
 app.use(cors());
 app.use(express.json());
+
+// middleware concept in action
+app.use((req, res, next) => {
+  console.log("Method: ", req.method)
+  console.log("Path: ", req.path)
+  console.log("Body: ", req.body)
+  console.log("---")
+
+  next();
+})
 
 let notes = [
     {
@@ -82,6 +94,11 @@ app.post("/notes/", (req, res) => {
   notes.push(newContent)
 
   res.status(204).json(newContent)
+})
+
+// if we try to access invalid route this middleware will responsible to handle it properly
+app.use((req, res, next) => {
+  res.status(404).send("<h1>No such routes detected.</h1>")
 })
 
 app.listen(Port, () => {
