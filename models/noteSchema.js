434e0d2b0f1f9adf8cf2b28a413562e6c5ab6a11
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-07-30 13:41:11 +0545

    update: post method updated to mongodb

--- models/noteSchema.js
+++ models/noteSchema.js
@@ -1,4 +1,18 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const url = process.env.MONGO_URL;
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("MongoDB connection established...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const noteSchema = new mongoose.Schema({
   content: {
--- server.js
+++ server.js
@@ -12,11 +12,12 @@
 //     console.log(`Server running on port http://localhost:${Port}`)
 // })
 
-
 // using express framework
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+
+const Note = require("./models/noteSchema");
 
 dotenv.config();
 
@@ -24,92 +25,130 @@ const app = express();
 
 const Port = process.env.PORT || 3001;
 
-
 // all this use things are middlewares
 
 // we have to use middleware in server code where we need to pass callback function which goes like - express.static("build") to run
 // both react app and server in same url --- before doing it we have to create build folder by commanding "npm run build"
 // in react app and it should be copied down to server's root directory.
 
-// what all this particular middleware does is check for executable file and execute or render it on browser before 
+// what all this particular middleware does is check for executable file and execute or render it on browser before
 // checking for other codes in backend server
-app.use(express.static("build"))   
+app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
 
 // middleware concept in action
 app.use((req, res, next) => {
-  console.log("Method: ", req.method)
-  console.log("Path: ", req.path)
-  console.log("Body: ", req.body)
-  console.log("---")
+  console.log("Method: ", req.method);
+  console.log("Path: ", req.path);
+  console.log("Body: ", req.body);
+  console.log("---");
 
   next();
-})
-
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy to learn",
-      date: "2022-1-17T17:30:31.098Z",
-      important: true,
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      date: "2022-1-17T18:39:34.091Z",
-      important: false,
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-1-17T19:20:14.298Z",
-      important: true,
-    },
-  ];
-
-app.get("/", (req, res) => {
-    res.send("hello world")
 });
 
-app.get("/notes", (req, res) => {
-    // res.status(200).json(notes)
-    res.json(notes)
+// let notes = [
+//     {
+//       id: 1,
+//       content: "HTML is easy to learn",
+//       date: "2022-1-17T17:30:31.098Z",
+//       important: true,
+//     },
+//     {
+//       id: 2,
+//       content: "Browser can execute only JavaScript",
+//       date: "2022-1-17T18:39:34.091Z",
+//       important: false,
+//     },
+//     {
+//       id: 3,
+//       content: "GET and POST are the most important methods of HTTP protocol",
+//       date: "2022-1-17T19:20:14.298Z",
+//       important: true,
+//     },
+//   ];
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/notes", (req, res, next) => {
+  // USING ARRAY AS DUMMY DB
+  // res.status(200).json(notes)
+
+  // USING MONGO AS DB
+  Note.find()
+    .then((allNotes) => {
+      res.status(200).json(allNotes);
+    })
+    .catch((err) => next(err));
 });
 
 app.get("/notes/:id", (req, res) => {
   const currentId = Number(req.params.id);
-  const thisNote = notes.find((note) => note.id === currentId)
+  const thisNote = notes.find((note) => note.id === currentId);
 
-  if(thisNote) res.status(200).json(thisNote)
-  else res.status(404).json({err : "404 not found", msg : `there is no note with ${currentId}`})
-})
+  if (thisNote) res.status(200).json(thisNote);
+  else
+    res.status(404).json({
+      err: "404 not found",
+      msg: `there is no note with ${currentId}`,
+    });
+});
 
 app.delete("/notes/:id", (req, res) => {
   const currentId = Number(req.params.id);
-  notes = notes.filter((note) => note.id !== currentId)
+  notes = notes.filter((note) => note.id !== currentId);
 
-  res.status(204).json({msg : "note successfully deleted"})
+  res.status(204).json({ msg: "note successfully deleted" });
 
   // res.status(204).end();
-})
+});
 
-app.post("/notes/", (req, res) => {
-  const newContent = req.body;
-  newContent.id = notes.length + 1;
-  newContent.date = new Date()
+app.post("/notes/", (req, res, next) => {
+  // USING STATIC ARRAY AS DB
+  // const newContent = req.body;
+  // newContent.id = notes.length + 1;
+  // newContent.date = new Date()
 
-  notes.push(newContent)
+  // notes.push(newContent)
 
-  res.status(204).json(newContent)
-})
+  // res.status(204).json(newContent)
+
+  // USING MONGO AS DB
+  const newNote = new Note({
+    content: req.body.content,
+    important: req.body.important || false,
+    date: new Date(),
+  });
+
+  newNote
+    .save()
+    .then((savedNewNote) => {
+      res.status(200).json(savedNewNote);
+    })
+    .catch((err) => next(err));
+});
 
 // if we try to access invalid route this middleware will responsible to handle it properly
 app.use((req, res, next) => {
-  res.status(404).send("<h1>No such routes detected.</h1>")
-})
-
-app.listen(Port, () => {
-    console.log(`Server running on port http://localhost:${Port}`)
+  res.status(404).send("<h1>No such routes detected.</h1>");
 });
 
+const errHandler = (err, req, res, next) => {
+  console.error(err.message);
+
+  if (err.name === "CastError") {
+    return res.status(400).send({ err: "malformatted id" });
+  } else if (err.name === "ValidationError") {
+    return res.status(400).json({ err: err.message });
+  }
+
+  next(err);
+};
+
+app.use(errHandler);
+
+app.listen(Port, () => {
+  console.log(`Server running on port http://localhost:${Port}`);
+});
